c292b1d75ba604e5a94a0624c63420e9c4fb085b
itsariadust <jakobaindreas11@gmail.com>
2024-12-10 19:26:38 +0800

    More refactoring

--- delete_item.py
+++ delete_item.py
@@ -7,6 +7,15 @@ class DeleteItem:
     def __init__(self):
         self.db = InventoryDB()
 
-    def item_remover(self):
+    def remove_item(self):
         item_name = inquirer.text(message="Enter the item's name")
-        self.db.delete_item(item_name)
+        items = self.db.get_items_by_name(item_name)
+
+        if not items:
+            print(f"No items found with the name '{item_name}'.")
+            return
+
+        if len(items) > 1:
+            self.db.handle_duplicates(items)
+        else:
+            self.db.delete_item_by_name(items[0])
